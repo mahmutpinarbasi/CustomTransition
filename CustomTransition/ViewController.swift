20273f79d6c51d129252300a59f5e92fa15c7976
Mahmut Pinarbasi <mahmut.pinarbasi@mobisem.com>
2017-10-15 15:41:50 +0300

    Initial Commit

---
+++ CustomTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTransition
+//
+//  Created by Sem0043 on 15/10/2017.
+//  Copyright © 2017 Mahmut Pinarbasi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
